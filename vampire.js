f77761d5742273850307b918c4ab9059f25bb69f
KateIsabelle <kate.davis887@gmail.com>
2021-03-21 01:31:13 +0000

    write vampire lineage functions and test with mocha chai

--- vampire.js
+++ vampire.js
@@ -6,26 +6,33 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
-
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const thisVampSeniority = this.numberOfVampiresFromOriginal;
+    const thatVampSeniority = vampire.numberOfVampiresFromOriginal;
+    return thisVampSeniority < thatVampSeniority;
   }
 
   /** Stretch **/
@@ -36,7 +43,6 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
   }
 }
 
